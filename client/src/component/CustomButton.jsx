3b4c4fa34d691ebbbc52b027017718a2a802f958
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-09 21:37:18 -0700

    customizer import component

--- client/src/component/CustomButton.jsx
+++ client/src/component/CustomButton.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 
-const CustomButton = () => {
+const CustomButton = ({ type,customStyles,handleClick,title }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if(type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#fff'
+      }
+    }
+  }
   return (
-    <div>CustomButton</div>
+   <button style={generateStyle(type)} className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} onClick={handleClick}>
+      {title}
+   </button>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,15 +25,14 @@ const Home = () => {
          <motion.div {...headTextAnimation}>
           <h1 className='head-text'>LET'S <br className='xl:block hidden'/> DO IT.</h1>
          </motion.div>
-         <motion.div {...headContainerAnimation} className='flex flex-col gap-5'>
+         <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
           <p className='max-w-md font-normal text-gray-600 text-base'>
            Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "}
            and define your own style .
           </p>
 
-          <CustomButton type="filled" title="Customize It " handleClick ={() => state.intro = false}>
-             
-          </CustomButton>
+          <CustomButton type="filled" title="Customize It " handleClick ={() => state.intro = false} customStyles='w-fit px-4 py-2.5 font-bold text-sm'/> 
+    
          </motion.div>
         </motion.div>
       </motion.section>
