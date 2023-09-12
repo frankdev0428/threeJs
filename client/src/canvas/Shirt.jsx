5ff52b10180a56a140ed2fc7165195b60b717307
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-11 20:53:16 -0700

    public folder modify

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { easing } from 'maath';
+import { useSnapshot  } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import {Decal, useGLTF , useTexture } from '@react-three/drei';
+import state from '../store';
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes,materials } = useGLTF('/shirt_baked.glb');
+  const logoTextture = useTexture(snap.logoDecal);
+  const fullTextture = useTexture(snap.fullDecal);
+  return (
+    <group>
+     <mesh
+      castShadow
+      geometry={nodes.T_shirt_male.geometry}
+      material={materials.lambert1}
+      material-roughless={1}
+      dispose={null}
+     ></mesh>
+    </group>
+  )
+}
+
+export default Shirt
