097301acbc2652d51eab2cc1015fc66cf98d747e
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-11 18:23:46 -0700

    Customizer filtertab implementation

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -7,7 +7,7 @@ import state from '../store';
 import { download } from '../assets';
 import { downloadCanvasToImage,reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes  } from '../config/constants';
-import { faceAnimation , slideAnimation } from '../config/motion';
+import { fadeAnimation , slideAnimation } from '../config/motion';
 
 const Customizer = () => {
   const snap = useSnapshot(state);
@@ -15,7 +15,34 @@ const Customizer = () => {
     <AnimatePresence>
       {!snap.intro && (
         <>
-        Customizer
+         <motion.div key='custom' className='absolute top-0 left-0 z-10'
+         {...slideAnimation('left')}>
+          <div className='flex items-center min-h-screen'>
+            <div className='editortabs-container tabs'>
+              {EditorTabs.map((tab) => (
+                <Tab
+                key={tab.name}
+                tab={tab}
+                handleClick={() => {}}
+                />
+              ))}
+            </div>
+          </div>
+         </motion.div>
+         <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+          <CustomButton type='filled' title="Go Back" handleClick={() => state.intro =true} customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
+         </motion.div>
+         <motion.div className='filtertabs-container' {...slideAnimation('up')}>
+         {FilterTabs.map((tab) => (
+                <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab=''
+                isActiveTab=''
+                handleClick={() => {}}
+                />
+              ))}
+         </motion.div>
         </>
       )}
     </AnimatePresence>
