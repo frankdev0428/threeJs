7a17c313e4b2959069b94f62ff2600915f674dcb
frankdev0428@gmail.com <frankdev0428@gmail.com>
2023-09-12 16:09:13 -0700

    RandomizedLight

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -4,11 +4,30 @@ import { useFrame } from '@react-three/fiber';
 import { AccumulativeShadows , RandomizedLight } from '@react-three/drei'
 
 const Backdrop = () => {
+  const shadows = useRef();
+
   return (
     <AccumulativeShadows 
       position={[ 0, 0 , -0.14]}
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scae={10}
+      rotation={[Math.PI / 2, 0, 0]}
       >
-      <RandomizedLight amount={4} />
+      <RandomizedLight
+      radius={9}
+      intensity={0.55}
+      ambient={0.25}
+      position= {[ 5, 5, -10]}
+      amount={4} />
+       <RandomizedLight
+      radius={5}
+      intensity={0.25}
+      ambient={0.55}
+      position= {[ 5, 5, -10]}
+      amount={4} />
     </AccumulativeShadows>
   )
 }
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -8,6 +8,8 @@ const CanvasModel = () => {
     <Canvas
       shadows
       camera={{ position: [0,0,0] , fov: 25 }}
+      gl={{ preserveDrawingBuffer: true}}
+      className = "w-full max-w-full h-full transition-all ease-in"
     >
      <ambientLight intensity={0.5} />
      <Environment preset='city' />
